8baac82a92cec261a24785a8e802aecc68d733f2
arjungit07 <139776248+arjungit07@users.noreply.github.com>
2024-09-04 16:06:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(
   cors({
     origin: process.env.FRONTEND_URL,
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"];
+    methods: ["GET", "POST", "PUT", "DELETE"]
   })
 );
 
