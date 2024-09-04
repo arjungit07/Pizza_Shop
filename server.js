28a605800822d1fbabddae02cea14b55f4cde696
arjungit07 <139776248+arjungit07@users.noreply.github.com>
2024-09-04 17:12:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -22,16 +22,14 @@ const app = express();
 //middlewares;
 app.use(morgan("dev"));
 
-app.use(
-  cors({
-    origin: process.env.FRONTEND_URL,
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"]
-  })
-);
+app.use(cors({
+  origin: ['https://online-pizza-delivery-app.vercel.app', 'http://localhost:3000'],  // Allow both Vercel and local development
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify the allowed HTTP methods
+  credentials: true, // Include credentials if needed (e.g., cookies or authentication headers)
+}));
 
 app.use(function (req, res, next) {	
-    res.setHeader('Access-Control-Allow-Origin', process.env.FRONTEND_URL,);    
+    res.setHeader('Access-Control-Allow-Origin', 'https://online-pizza-delivery-app.vercel.app',);    
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');    
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');   
     res.setHeader('Access-Control-Allow-Credentials', true);    
